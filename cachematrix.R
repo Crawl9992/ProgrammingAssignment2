677cb9ce7f65ec9829d47509615fd89b32dcb0c0
Eric Smith <crawl9992@hotmail.com>
2015-05-18 22:01:06 -0700

    Added bunch of comments;

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions will inverse a matrix and cache the inversed matirx in a list.
+## runCacheMatrix tests the makeCacheMatrix and cacheSolve functions
 
-## Write a short comment describing this function
 
+## makeCacheMatrix creates a special "matrix" (a list) containing functions to:
+#### 1.set the value of the matrix (get)
+#### 2.get the value of the matrix (set)
+#### 3.set the value of the inversed matrix (setinverse) 
+#### 4.get the value of the inversed matrix (getinverse) 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -12,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() x
     setinverse <- function(inverse) i <<- inverse
     getinverse <- function() i
+    
+    # return the list
     list(set = set, get = get,
         setinverse = setinverse,
         getinverse = getinverse)
@@ -19,49 +25,62 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve will calculates and return the inverse of the special "matrix" (a list).
+## If the inverse has already been calculated, it gets the inversed matrix from the cache and skips the computation.
+## Otherwise, it calculates the inversed matrix and sets the value from the computation.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        i <- x$getinverse()
+  
+        ## Check if the inverse of 'x' has been cached
+        i <- x$getinverse()        
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+        
+        ## Calculate the inverse of 'x', cache the inverse, and return it.
         data <- x$get()
         i <- solve(data, ...)
         x$setinverse(i)
         i
 }
 
-runCacheMatrix <- function() {
-  #create a sqaure matrix 2 to the 2nd power (2^2)
-  h4<-rbind(c(1, -1/4), c(-1/4, 1))
-  message("2^2 matrix")
-  print(h4)
 
-  #create a sqaure matrix 2 to the 3rd power (2^3)
+## runCacheMatrix tests the makeCacheMatrix and cacheSolve functions with a 2x2 matrix and 8x8 matrix 
+runCacheMatrix <- function() {
+  #create a sqaure matrix 2 by 2 (2x2)
+  h2<-rbind(c(1, -1/4), c(-1/4, 1))
+  message("2x2 matrix")
+  print(h2)
+
+  #create a sqaure matrix 8 by 8 (8x8)
   hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
   h8 <- hilbert(8)
-  message("2^3 matrix")
+  message("8x8 matrix")
   print(h8)
   
   
-  #cache the h4 (2^2)
-  message("inverse and cache 2^2 matrix")
-  h4Cache<-makeCacheMatrix(h4)
+  #cache the h2 (2x2)
+  message("inverse and cache 2x2 matrix")
+  h2Cache<-makeCacheMatrix(h2)
 
   #Run cacheSolve twice; first call caches the value; second call returns the cached value
-  cacheSolve(h4Cache)
-  print(cacheSolve(h4Cache))
+  cacheSolve(h2Cache)
+  print(cacheSolve(h2Cache))
 
 
-  #cache the h8 (2^3)
-  message("inverse and cache 2^3 matrix")
+  #cache the h8 (8x8)
+  message("inverse and cache 8x8 matrix")
   h8Cache<-makeCacheMatrix(h8)
   
   #Run cacheSolve twice; first call caches the value; second call returns the cached value
   cacheSolve(h8Cache)
   print(cacheSolve(h8Cache))
   
+  
+  # Confirm the matrix is an Identify Matirx
+  message("2x2 Identify Matirx")
+  print(round(h2Cache$getinverse() %*% h2Cache$get(), 3))
+  message("8x8 Identify Matirx")
+  print(round(h8Cache$getinverse() %*% h8Cache$get(), 3))
 }
